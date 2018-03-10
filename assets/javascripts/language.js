f8f84c58ae1c4d7d6bdb7b53a0e92e2f8a361ee1
JackyGuo <jackyguo@me.com>
2018-03-10 23:42:49 +0800

    v0.1 - 网站初始化

---
+++ assets/javascripts/language.js
@@ -0,0 +1,33 @@
+var name = "language";
+
+function  chgLang(value) {
+    SetCookie(name,value);
+    location.reload();
+}
+function  SetCookie(name,value){
+    var Days = 30; //此 cookie 将被保存 30 天
+    var exp = new Date();    //new Date("December 31, 9998");
+    exp.setTime(exp.getTime() + Days*24*60*60*1000);
+    document.cookie = name + "="+ escape (value) + "; expires=" + exp.toGMTString();
+}
+function getCookie(name)//取cookies函数
+{
+
+    var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
+    if(arr != null) return unescape(arr[2]); return null
+
+}
+
+$(function() {
+
+    if (getCookie(name) != "") {
+        if (getCookie(name) == "cn") {
+            $("[data-localize]").localize("text", {pathPrefix: "lang", language: "cn"});
+        }
+        if (getCookie(name) == "en") {
+            $("[data-localize]").localize("text", {pathPrefix: "lang", language: "en"});
+        }
+    }
+    //根据cookie选择语言
+
+});
